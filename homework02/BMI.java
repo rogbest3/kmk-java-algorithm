8214e0d74e219e5542bece595fdc6514c4dcd57e
rogbest3 <rogbest3@gmail.com>
2019-08-14 18:00:31 +0900

    190814 18:00

--- homework02/BMI.java
+++ homework02/BMI.java
@@ -4,7 +4,7 @@ public class BMI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	 /**
+	/**
 	  * 사람의 이름을 입력하고, 몸무게 , 키를 입력하면
 		해당 사람의 상태가 비만인지 저체중인지 알려주는 BMI 지수 측정하는 프로그램을 만들어 주세요.
 		BMI 구하는 공식은 아래와 같아요. 더 궁금하면 여기를 클릭하세요.
--- homework02/MonthEndDay.java
+++ homework02/MonthEndDay.java
@@ -4,7 +4,12 @@ public class MonthEndDay {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/**
+		 * 월을 입력하면 해당 월이 몇일까지인지
+			알려주는 프로그램을 작성해 주세요.
+			단) 2월은 윤년을 따지지 않고 무조건 29일입니다.
+			출력문장 : "??월은 **일 까지 입니다."
+		 */
 	}
 
 }
